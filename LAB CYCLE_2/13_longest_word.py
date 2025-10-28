cd8c9bbe9962dc2ed5fe2cbf58c8f81fe605783b
Vignesh <onlinedel2023@gmail.com>
2025-10-28 15:31:27 +0530

    Add files via upload

---
+++ LAB CYCLE_2/13_longest_word.py
@@ -0,0 +1,9 @@
+word_list = input("Enter a list of words (comma-separated):").split(',')
+long_len = 0
+for word in word_list:
+    word = word.strip()
+    curr_len = len(word)
+    if curr_len > long_len:
+        long_len = curr_len
+        long_word = word
+print(f"Longest word: '{long_word}' with length {long_len}")
